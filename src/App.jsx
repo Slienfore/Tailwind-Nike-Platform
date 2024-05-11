8dd4cc7ec643e434202f0e123ed789b2040811fe
Slienfore <Slienfore@163.com>
2024-05-11 20:01:17 +0800

    Building the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-const App = () => {
+const App = () => (
+  <>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">PopularProducts</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding padding-x py-10">Services</section>
+      <section className="bg-pale-blue padding">CustomReviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
+  </>
+);
 
-}
-
-export default App
+export default App;

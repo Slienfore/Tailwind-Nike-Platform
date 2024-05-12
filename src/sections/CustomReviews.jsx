045ced85cf0d236fbedaf68ed6fade36c4a318a1
Slienfore <Slienfore@163.com>
2024-05-12 19:23:09 +0800

    Building Customer Reviews Section

--- src/sections/CustomReviews.jsx
+++ src/sections/CustomReviews.jsx
@@ -1,5 +1,33 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+
 const CustomReviews = () => {
-  return <div>CustomerReviews</div>;
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red">&nbsp;Customers&nbsp;</span>
+        Say?
+      </h3>
+
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+
+      <div className="flex-1 mt-24 flex max-lg:flex-col justify-evenly items-center gap:14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          ></ReviewCard>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomReviews;

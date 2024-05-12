7f7758daf7362c9c04a0766a0882c5fb33e58dee
Slienfore <Slienfore@163.com>
2024-05-12 10:41:34 +0800

    Building the NavBar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import { headerLogo } from "../assets/images";
+// import { hamburger } from "../assets/images/icons";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img width={130} height={29} src={headerLogo} alt="Logo" />
+        </a>
+
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-monts errat leading-normal text-lg text-slate-gary"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
